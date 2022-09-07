d72512b905aaba84ba7feb9d2ea1d015cf8931b9
Xavier Ngaba <xavierngaba@gmail.com>
2022-09-07 20:29:52 +0200

    add presence du bot

--- index.js
+++ index.js
@@ -3,5 +3,15 @@ const Discord = require("discord.js");
 const { Client, GatewayIntentBits  } = require('discord.js');
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
+// on attends que le bot soit prêt
+client.on("ready", () => {
+    // On change le statut
+    client.user.setPresence({ 
+        activities: { 
+            name: 'En pleine démo' 
+        }, 
+        status: 'dnd' })
+});
+
 // On se connecte
-client.login("OTU0MDM4MzA1MjQxMDY3NTIx.GTkMfW.cJoCjjFD2OZ7T-nNf0UXuZOE44Zq-Hi4LZ3hf8");
+client.login("OTU0MDM4MzA1MjQxMDY3NTIx.GIFujq.DsAiAN7y59zePMx2Kwnuq6YVY3osJbJImDZLLQ");
